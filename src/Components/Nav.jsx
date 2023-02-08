0af1bddff83fbf46f48d327510933bf5ac694f5d
AllenDodul <109014717+AllenDodul@users.noreply.github.com>
2023-02-08 18:28:59 +0600

    1 file modified, 1 added

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import '../Styles/Nav.css';
 
 const Nav = ()=>{
   return(
-    <div>
-      <img src="https://i.ibb.co/D41z0K6/Facebook-logo.png" alt="Facebook-logo"/>
-      <i className="fas fa-search"></i>
+    <div className='nav_bar'>
+      <div className='nav_left'>
+        <div className='nav_logo'>
+          <img 
+            className="facebook_logo"
+            src="https://i.ibb.co/D41z0K6/Facebook-logo.png" 
+            alt="Facebook-logo"
+          />
+        </div>
+        <div className='nav_search_box'>
+          <input type='text' placeholder='Search Here'/>
+          <i className="fas fa-search"></i>
+        </div>
+      </div>
+      <div className='nav_canter'>
+        <div className='nav_canter_logo'>
+          <i class="fas fa-home"></i>
+          
+        </div>
+      </div>
     </div>
   )
 }
